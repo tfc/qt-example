d22be0e5580fee6f00bc2eb879ea00cd244bf706
Jacek Galowicz <jacek@galowicz.de>
2023-01-06 10:33:37 +0100

    Enable multiple platforms

--- flake.nix
+++ flake.nix
@@ -1,30 +1,32 @@
 {
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { self, nixpkgs }:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
-  in {
-    packages.x86_64-linux.default = pkgs.qt6Packages.callPackage ./build.nix {};
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      inputsFrom = [ self.packages.x86_64-linux.default ];
-      buildInputs = with pkgs; [
-        gdb
-        qtcreator
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "aarch64-linux" "x86_64-linux" ];
+      perSystem = { config, pkgs, ... }: {
+        packages.default = pkgs.qt6Packages.callPackage ./build.nix { };
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ config.packages.default ];
+          buildInputs = with pkgs; [
+            gdb
+            qtcreator
 
-        # this is for the shellhook portion
-        qt6.wrapQtAppsHook
-        makeWrapper
-        bashInteractive
-      ];
-      # set the environment variables that unpatched Qt apps expect
-      shellHook = ''
-        bashdir=$(mktemp -d)
-        makeWrapper "$(type -p bash)" "$bashdir/bash" "''${qtWrapperArgs[@]}"
-        exec "$bashdir/bash"
-      '';
+            # this is for the shellhook portion
+            qt6.wrapQtAppsHook
+            makeWrapper
+            bashInteractive
+          ];
+          # set the environment variables that unpatched Qt apps expect
+          shellHook = ''
+            bashdir=$(mktemp -d)
+            makeWrapper "$(type -p bash)" "$bashdir/bash" "''${qtWrapperArgs[@]}"
+            exec "$bashdir/bash"
+          '';
+        };
+      };
     };
-  };
 }
